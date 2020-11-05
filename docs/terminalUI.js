37e1b3e48ec1769207600da67c6c4b487ed2f981
LeumasMymik <samuel.kim.475@my.csun.edu>
2020-11-05 11:28:12 -0800

    edit main page

---
+++ docs/terminalUI.js
@@ -0,0 +1,11 @@
+$('#terminal').terminal({
+	    hello: function(what) {
+		            this.echo('Hello, ' + what +
+				                      '. Welcome to this terminal.');
+		        }
+}, {
+	    greetings: 'Connected to LeumasMymik.Github.io',
+	    height: 400,
+	    prompt: 'guest@leumasmymik.github.io:~$ '
+});
+
